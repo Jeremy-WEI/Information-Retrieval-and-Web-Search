beabc5bd075c0083a42e9a5ef9112817674da925
Yunchen Wei <weiyunchen0807@outlook.com>
2016-06-17 20:23:37 -0700

    LanguageModel.java

--- pa2-skeleton/src/edu/stanford/cs276/CandidateGenerator.java
+++ pa2-skeleton/src/edu/stanford/cs276/CandidateGenerator.java
@@ -6,31 +6,30 @@ import java.util.Set;
 
 public class CandidateGenerator implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-	private static CandidateGenerator cg_;
+    private static final long serialVersionUID = 1L;
+    private static CandidateGenerator cg_;
 
-  // Don't use the constructor since this is a Singleton instance
-  private CandidateGenerator() {}
-
-  public static CandidateGenerator get() throws Exception {
-    if (cg_ == null) {
-      cg_ = new CandidateGenerator();
+    // Don't use the constructor since this is a Singleton instance
+    private CandidateGenerator() {
     }
-    return cg_;
-  }
 
-  public static final Character[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f',
-      'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-      'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7',
-      '8', '9', ' ', ',' };
+    public static CandidateGenerator get() throws Exception {
+        if (cg_ == null) {
+            cg_ = new CandidateGenerator();
+        }
+        return cg_;
+    }
 
-  // Generate all candidates for the target query
-  public Set<String> getCandidates(String query) throws Exception {
-    Set<String> candidates = new HashSet<String>();
-    /*
-     * Your code here
-     */
-    return candidates;
-  }
+    public static final Character[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+            'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', ',' };
+
+    // Generate all candidates for the target query
+    public Set<String> getCandidates(String query) throws Exception {
+        Set<String> candidates = new HashSet<String>();
+        /*
+         * Your code here
+         */
+        return candidates;
+    }
 
 }
--- pa2-skeleton/src/edu/stanford/cs276/LanguageModel.java
+++ pa2-skeleton/src/edu/stanford/cs276/LanguageModel.java
@@ -19,84 +19,119 @@ import edu.stanford.cs276.util.Dictionary;
  * (https://en.wikipedia.org/wiki/Singleton_pattern).
  */
 public class LanguageModel implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
-  private static LanguageModel lm_;
 
-  Dictionary unigram = new Dictionary();
+    private static final long serialVersionUID = 1L;
+    private static LanguageModel lm_;
 
-  /*
-   * Feel free to add more members here (e.g., a data structure that stores bigrams)
-   */
+    private static double lambda = 0.0;
+    private Dictionary unigram = new Dictionary();
+    private Dictionary bigram = new Dictionary();
 
-  /**
-   * Constructor
-   * Do not call constructor directly from outside this class since this is a Singleton class
-   */
-  private LanguageModel(String corpusFilePath) throws Exception {
-    constructDictionaries(corpusFilePath);
-  }
+    /*
+     * Feel free to add more members here (e.g., a data structure that stores
+     * bigrams)
+     */
 
-  /**
-   * This method computes language model parameters (i.e. counts of unigrams, bigrams etc)
-   */
-  public void constructDictionaries(String corpusFilePath) throws Exception {
-
-    System.out.println("Constructing dictionaries...");
-    File dir = new File(corpusFilePath);
-    for (File file : dir.listFiles()) {
-      if (".".equals(file.getName()) || "..".equals(file.getName())) {
-        continue; // Ignore the self and parent aliases.
-      }
-      System.out.printf("Reading data file %s ...\n", file.getName());
-      BufferedReader input = new BufferedReader(new FileReader(file));
-      String line = null;
-      while ((line = input.readLine()) != null) {
-        /*
-         * Remember: each line is a document (refer to PA2 handout)
-         * TODO: Your code here
-         */
-        unigram.add("cs276");
-      }
-      input.close();
+    /**
+     * Constructor Do not call constructor directly from outside this class
+     * since this is a Singleton class
+     */
+    private LanguageModel(String corpusFilePath) throws Exception {
+        constructDictionaries(corpusFilePath);
     }
-    System.out.println("Done.");
-  }
 
-  /**
-   * Loads the language model object (and all associated data) from disk
-   */
-  public static LanguageModel load() throws Exception {
-    try {
-      if (lm_ == null) {
-        FileInputStream fiA = new FileInputStream(Config.languageModelFile);
-        ObjectInputStream oisA = new ObjectInputStream(fiA);
-        lm_ = (LanguageModel) oisA.readObject();
-      }
-    } catch (Exception e) {
-      throw new Exception("Unable to load language model.  You may have not run build corrector");
+    private String getBigramString(String term1, String term2) {
+        return term1 + " " + term2;
     }
-    return lm_;
-  }
 
-  /**
-   * Saves the object (and all associated data) to disk
-   */
-  public void save() throws Exception {
-    FileOutputStream saveFile = new FileOutputStream(Config.languageModelFile);
-    ObjectOutputStream save = new ObjectOutputStream(saveFile);
-    save.writeObject(this);
-    save.close();
-  }
+    /**
+     * This method computes language model parameters (i.e. counts of unigrams,
+     * bigrams etc)
+     */
+    public void constructDictionaries(String corpusFilePath) throws Exception {
 
-  /**
-   * Creates a new LanguageModel object from a corpus. This method should be used to create a
-   * new object rather than calling the constructor directly from outside this class
-   */
-  public static LanguageModel create(String corpusFilePath) throws Exception {
-    if (lm_ == null) {
-      lm_ = new LanguageModel(corpusFilePath);
+        System.out.println("Constructing dictionaries...");
+        File dir = new File(corpusFilePath);
+        for (File file : dir.listFiles()) {
+            if (".".equals(file.getName()) || "..".equals(file.getName()) || file.isHidden()) {
+                continue; // Ignore the self and parent aliases.
+            }
+            System.out.printf("Reading data file %s ...\n", file.getName());
+            BufferedReader input = new BufferedReader(new FileReader(file));
+            String line = null;
+            while ((line = input.readLine()) != null) {
+                /*
+                 * Remember: each line is a document (refer to PA2 handout)
+                 * TODO: Your code here
+                 */
+                String[] tokens = line.split("\\s+");
+                for (int i = 0; i < tokens.length - 1; i++) {
+                    unigram.add(tokens[i]);
+                    bigram.add(getBigramString(tokens[i], tokens[i + 1]));
+                }
+                unigram.add(tokens[tokens.length - 1]);
+            }
+            input.close();
+        }
+        System.out.println("Done.");
     }
-    return lm_;
-  }
+
+    public static void setSmoothingLambda(double lambda) {
+        LanguageModel.lambda = lambda;
+    }
+
+    public double getUnigramProb(String term) {
+        return (double) unigram.count(term) / unigram.termCount();
+    }
+
+    public double getBigramProb(String term1, String term2) {
+        return lambda * getUnigramProb(term1) + (1 - lambda) * bigram.count(getBigramString(term1, term2)) / unigram.count(term1);
+    }
+
+    /**
+     * Loads the language model object (and all associated data) from disk
+     */
+    public static LanguageModel load() throws Exception {
+        try {
+            if (lm_ == null) {
+                FileInputStream fiA = new FileInputStream(Config.languageModelFile);
+                ObjectInputStream oisA = new ObjectInputStream(fiA);
+                lm_ = (LanguageModel) oisA.readObject();
+            }
+        } catch (Exception e) {
+            throw new Exception("Unable to load language model.  You may have not run build corrector");
+        }
+        return lm_;
+    }
+
+    /**
+     * Saves the object (and all associated data) to disk
+     */
+    public void save() throws Exception {
+        FileOutputStream saveFile = new FileOutputStream(Config.languageModelFile);
+        ObjectOutputStream save = new ObjectOutputStream(saveFile);
+        save.writeObject(this);
+        save.close();
+    }
+
+    /**
+     * Creates a new LanguageModel object from a corpus. This method should be
+     * used to create a new object rather than calling the constructor directly
+     * from outside this class
+     */
+    public static LanguageModel create(String corpusFilePath) throws Exception {
+        if (lm_ == null) {
+            lm_ = new LanguageModel(corpusFilePath);
+        }
+        return lm_;
+    }
+
+    //    public static void main(String[] args) throws Exception {
+    //        //        LanguageModel lm = new LanguageModel("/Users/YunchenWei/Documents/Coursera/IR/pa2-data/corpus");
+    //        LanguageModel.load();
+    //        LanguageModel lm = create("/Users/YunchenWei/Documents/Coursera/IR/pa2-data/corpus");
+    //        System.out.println(lm.getUnigramProb("test"));
+    //        System.out.println(lm.getUnigramProb("i"));
+    //        System.out.println(lm.getBigramProb("i", "want"));
+    //    }
 }
